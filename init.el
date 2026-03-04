f368100c07c666a23e2e70261fdad28445c99f6a
Dilapidated Realism <no>
2026-03-04 01:04:31 -0600

    First Commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(org-babel-load-file
+ (expand-file-name "/home/littlefear/.emacs.d/uwu.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(leuven-dark))
+ '(org-cycle-emulate-tab t)
+ '(org-support-shift-select 'always)
+ '(package-selected-packages
+   '(company flycheck helm-lsp lsp-java lsp-ui org-roam-timeline
+	     org-roam-ui slime sqlite3)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
